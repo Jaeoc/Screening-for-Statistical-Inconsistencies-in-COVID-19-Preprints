462b31475d5e39c3266513ccabda7746de16043e
Anton TiU <anton@olssoncollentine.com>
2020-11-27 10:10:09 +0100

    made sample_tab its own file

--- code/functions.R
+++ code/functions.R
@@ -1,5 +1,4 @@
 #********************************************************************************
-
 ##Project: Screening for statistical inconsistencies in COVID-19 preprints
 ##Script purpose: Source functions for checking consistency of reported results
 ##Code: 
@@ -379,25 +378,4 @@ split_check_bind <- function(x){
   out 
   
 }
-
-#********************************************************************************
-#Other functions----
-#********************************************************************************
-
-### Function to sample two tables that need to be extracted from a preprint
-sample_tab <- function(nr_tab, pp_ID)
-{
-  ### Arguments:
-  # - nr_tab = total number of tables in preprint
-  # - pp_ID = Preprint ID
-  
-  set.seed(pp_ID) # Seed based on preprint ID
-  
-  extr_tab <- sample(1:nr_tab, size = 2)
-  
-  print(paste0("Tables ", extr_tab[1], " and ", extr_tab[2], " need to be extracted"))
-}
-
-# ### Example
-# sample_tab(nr_tab = 4, pp_ID = 977)
 
